9e7d4230eaf9c9ad6cae93595cc2231406f58b4d
Sujaya Nadith <127515360+SnugCode@users.noreply.github.com>
2024-05-26 13:35:47 +0530

    new txt label

--- index.php
+++ index.php
@@ -13,20 +13,20 @@
         <h2>New Archer Registration</h2>
         <form action="process_new_archer.php" method="post">
             <div class="container">
-                <input type="text" id="firstName" name="firstName" class="input" required>
                 <label for="firstName" class="label">First Name:</label>
+                <input type="text" id="firstName" name="firstName" class="input" required>
             </div>
             <div class="container">
-                <input type="text" id="lastName" name="lastName" class="input" required>
                 <label for="lastName" class="label">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" class="input" required>
             </div>
             <div class="container">
-                <input type="date" id="dob" name="dob" class="input" required>
                 <label for="dob" class="label">Date of Birth:</label>
+                <input type="date" id="dob" name="dob" class="input" required>
             </div>
             <div class="container">
-                <input type="text" id="gender" name="gender" class="input" required>
                 <label for="gender" class="label">Gender:</label>
+                <input type="text" id="gender" name="gender" class="input" required>
             </div>
             <input type="submit" value="Register">
         </form>
@@ -36,8 +36,8 @@
         <h2>Existing Archer</h2>
         <form action="search_archer.php" method="post">
             <div class="container">
-                <input type="text" id="searchName" name="searchName" class="input" required>
                 <label for="searchName" class="label">Search Name:</label>
+                <input type="text" id="searchName" name="searchName" class="input" required>
             </div>
             <input type="submit" value="Search">
         </form>
